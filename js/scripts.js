2ff287991c382e3f4cc15fa4bb1469006483b002
johndavid25 <75648891+johndavid25@users.noreply.github.com>
2021-01-15 18:15:46 -0500

    major design changes

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+/*!
+    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+
+
+function FindSum(numbers, answer) {
+    let checkValue = 0;
+    let found = false;
+    for (let i = 0; i <= numbers.length - 1; i++) {
+        checkValue = answer - numbers[i];
+        if (numbers.includes(checkValue, i + 1)) {
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
+document.getElementById("btnSubmit").addEventListener("click", DisplaySum);
+
+function DisplaySum() {
+    let answer = parseInt(document.getElementById("kVariable").value);
+    let input1 = parseInt(document.getElementById("input1").value);
+    let input2 = parseInt(document.getElementById("input2").value);
+    let input3 = parseInt(document.getElementById("input3").value);
+    let input4 = parseInt(document.getElementById("input4").value);
+    let numArray = [input1, input2, input3, input4];
+
+    document.getElementById("numList").innerHTML = numArray.toString();
+    let found = FindSum(numArray, answer);
+    document.getElementById("results").innerHTML = found.toString();
+}
